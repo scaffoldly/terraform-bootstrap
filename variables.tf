e45a6ca8e9ce39ec73fb463583f97e103bbec231
Christian Nuss <cnuss@users.noreply.github.com>
2020-06-28 12:03:33 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,6 @@
 variable "GITHUB_TOKEN" {
   type        = string
   description = "GitHub Token"
-
-  # TODO: Remove this
-  default = "b06c7d38c4b388d2e431ee270a42260f1c72d7b7"
 }
 
 variable "GITHUB_REPOSITORY" {
