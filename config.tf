081fb3d229c5aac349aaddb55e2b62d8b35fe7c6
Christian Nuss <christian.nuss@gmail.com>
2020-07-23 21:23:34 -0700

    Missing Curly

--- config.tf
+++ config.tf
@@ -2,6 +2,7 @@ provider "github" {
   version      = "~> 2.8"
   token        = var.BOOTSTRAP_GITHUB_TOKEN
   organization = data.external.git.result.organization
+}
 
 provider "external" {
   version = "~> 1.2"
