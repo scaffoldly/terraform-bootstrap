ca8985b0c99daf2f0f709c361fd5049498f37daf
Christian Nuss <christian.nuss@gmail.com>
2020-07-23 21:21:13 -0700

    Bash + Templating Magic

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "BOOTSTRAP_GITHUB_TOKEN" {
   type        = string
   description = "GitHub Token"
 }
+
+variable "BOOTSTRAP_TERRAFORM_TOKEN" {
+  type        = string
+  description = "Terraform Cloud Team Token"
+}
