71ffe2d7f4a84b416ef90b53346f294a423654c6
Christian Nuss <christian.nuss@gmail.com>
2020-08-13 21:45:34 -0700

    Use for_each

--- aws-dns/main.tf
+++ aws-dns/main.tf
@@ -15,12 +15,20 @@ resource "aws_acm_certificate" "apex_wildcard" {
   }
 }
 
-resource "aws_route53_record" "apex_wildcard_validation" {
+resource "aws_route53_record" "apex_wildcard" {
+  for_each = {
+    for dvo in aws_acm_certificate.apex_wildcard.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
   zone_id = aws_route53_zone.zone.zone_id
-  name    = aws_acm_certificate.apex_wildcard.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.apex_wildcard.domain_validation_options.0.resource_record_type
-  ttl     = "60"
-  records = [aws_acm_certificate.apex_wildcard.domain_validation_options.0.resource_record_value]
 }
 
 resource "aws_acm_certificate" "subdomain_wildcard" {
@@ -32,10 +40,18 @@ resource "aws_acm_certificate" "subdomain_wildcard" {
   }
 }
 
-resource "aws_route53_record" "subdomain_wildcard_validation" {
+resource "aws_route53_record" "subdomain_wildcard" {
+  for_each = {
+    for dvo in aws_acm_certificate.subdomain_wildcard.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
   zone_id = aws_route53_zone.zone.zone_id
-  name    = aws_acm_certificate.subdomain_wildcard.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.subdomain_wildcard.domain_validation_options.0.resource_record_type
-  ttl     = "60"
-  records = [aws_acm_certificate.subdomain_wildcard.domain_validation_options.0.resource_record_value]
 }
