82c7c6978807109aad842db6619aafb2a294c79a
Christian Nuss <christian.nuss@gmail.com>
2020-09-04 00:46:34 -0400

    Make each domain individually

--- aws-api-gateway/main.tf
+++ aws-api-gateway/main.tf
@@ -56,29 +56,12 @@ resource "aws_api_gateway_account" "account" {
   cloudwatch_role_arn = aws_iam_role.api_gateway_cloudwatch.arn
 }
 
-# TODO: Wait for wildcard certificate ARN
-resource "aws_api_gateway_domain_name" "domain" {
+module "domain" {
+  source   = "./domain"
   for_each = var.stage_domains
 
-  security_policy = "TLS_1_2"
+  zone_id         = lookup(each.value, "zone_id", "unknown-zone-id")
+  domain          = lookup(each.value, "domain", "unknown-domain")
   certificate_arn = lookup(each.value, "wildcard_certificate_arn", "unknown-arn")
-  domain_name     = "api.${lookup(each.value, "domain", "unknown-domain")}"
-
-  tags = {
-    zone_id = lookup(each.value, "zone_id", "unknown-zone-id")
-  }
 }
 
-resource "aws_route53_record" "api_record" {
-  count = length(aws_api_gateway_domain_name.domain)
-
-  name    = aws_api_gateway_domain_name.domain[count.index].domain_name
-  type    = "A"
-  zone_id = aws_api_gateway_domain_name.domain[count.index].tags.zone_id
-
-  alias {
-    name                   = aws_api_gateway_domain_name.domain[count.index].domain_name_configuration[0].target_domain_name
-    zone_id                = aws_api_gateway_domain_name.domain[count.index].domain_name_configuration[0].hosted_zone_id
-    evaluate_target_health = false
-  }
-}
