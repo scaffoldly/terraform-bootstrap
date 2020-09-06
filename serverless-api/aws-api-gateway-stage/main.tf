08c1f18b4df3dfb156410fb10779a1f3762aeaca
Christian Nuss <christian.nuss@gmail.com>
2020-09-06 09:57:59 -0400

    Set the settings on the right stage

--- serverless-api/aws-api-gateway-stage/main.tf
+++ serverless-api/aws-api-gateway-stage/main.tf
@@ -116,7 +116,7 @@ resource "aws_api_gateway_stage" "stage" {
 
 resource "aws_api_gateway_method_settings" "settings" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = aws_api_gateway_deployment.deployment.stage_name
+  stage_name  = aws_api_gateway_stage.stage.stage_name
   method_path = "*/*"
 
   settings {
