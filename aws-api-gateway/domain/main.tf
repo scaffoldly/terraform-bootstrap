09a75c5b84c3aa9af4651810bb7d327b2d257835
Christian Nuss <christian.nuss@gmail.com>
2020-09-06 08:01:03 -0400

    Add evaluate_target_health

--- aws-api-gateway/domain/main.tf
+++ aws-api-gateway/domain/main.tf
@@ -15,7 +15,8 @@ resource "aws_route53_record" "api_record" {
   zone_id = var.zone_id
 
   alias {
-    name    = aws_api_gateway_domain_name.domain.cloudfront_domain_name
-    zone_id = aws_api_gateway_domain_name.domain.cloudfront_zone_id
+    evaluate_target_health = false
+    name                   = aws_api_gateway_domain_name.domain.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.domain.cloudfront_zone_id
   }
 }
