279fc2f166101f4fc7b7be6f0f4ffa09ba59a0ca
Christian Nuss <christian.nuss@gmail.com>
2020-09-06 08:30:40 -0400

    Create the default stage

--- serverless-api/aws-api-gateway-stage/main.tf
+++ serverless-api/aws-api-gateway-stage/main.tf
@@ -11,6 +11,10 @@ resource "aws_cloudwatch_log_group" "group" {
   name = "/aws/apigateway/${var.name}-${var.stage}"
 }
 
+resource "aws_cloudwatch_log_group" "execution_group" {
+  name = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.api.id}/${var.stage}"
+}
+
 resource "aws_api_gateway_gateway_response" "response_cors" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   response_type = "DEFAULT_4XX"
@@ -30,6 +34,51 @@ resource "aws_api_gateway_base_path_mapping" "mapping" {
   api_id      = aws_api_gateway_rest_api.api.id
   stage_name  = var.stage
   domain_name = "${var.subdomain}.${var.domain}"
+}
+
+resource "aws_api_gateway_resource" "health" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "health"
+}
+
+resource "aws_api_gateway_method" "health_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.health.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "health_get" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.health_get.id
+  http_method = aws_api_gateway_method.health_get.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.health_get]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = var.stage
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = var.stage
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
+
+  depends_on = [aws_cloudwatch_log_group.execution_group]
+}
+
+resource "aws_api_gateway_method_settings" "settings" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_stage.stage.stage_name
+  method_path = "${aws_api_gateway_resource.health.path_part}/${aws_api_gateway_method.health.http_method}"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
 }
 
 # TODO Health Check Resource + Logging + Etc
