cd3542df1c494e8e16abb52e582ad46ec7375f64
Christian Nuss <christian.nuss@gmail.com>
2020-09-09 22:31:44 -0500

    Rename variable

--- serverless-api/secrets/main.tf
+++ serverless-api/secrets/main.tf
@@ -1,15 +1,15 @@
 variable "repository_name" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "deployer_aws_access_key" {}
+variable "deployer_aws_secret_key" {}
 
 resource "github_actions_secret" "deployer_aws_access_key" {
   repository      = var.repository_name
   secret_name     = "AWS_ACCESS_KEY_ID"
-  plaintext_value = var.aws_access_key
+  plaintext_value = var.deployer_aws_access_key
 }
 
 resource "github_actions_secret" "deployer_aws_secret_key" {
   repository      = var.repository_name
   secret_name     = "AWS_SECRET_ACCESS_KEY"
-  plaintext_value = var.aws_secret_key
+  plaintext_value = var.deployer_aws_secret_key
 }
