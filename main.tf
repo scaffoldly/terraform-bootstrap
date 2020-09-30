bdab2e69e8842b86d1d4678766754a477c295113
Christian Nuss <christian.nuss@gmail.com>
2020-09-29 20:12:55 -0600

    Fix variable

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "aws_organization" {
   source = "./aws-organization"
   name   = data.external.git.result.organization
-  email  = local.email
+  email  = local.root_email
 }
 
 module "dns" {
