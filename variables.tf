60abc7a44be74c0c94dd074f6a8196852abbea4a
Christian Nuss <christian.nuss@gmail.com>
2020-10-06 22:20:08 -0700

    Variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,18 @@
 variable "root_email" {}
 variable "github_token" {}
 variable "organization" {}
+
 variable "stages" {
-  type = "map"
+  type = list
 }
+variable "nonlive" {
+  type = map
+}
+variable "live" {
+  type = map
+}
+
 variable "api_subdomain" {}
 variable "serverless_apis" {
-  type = "map"
+  type = map
 }
