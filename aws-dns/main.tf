6fa3b6586a3f1cc0831fdc9eaf0db1eb1f2ab84f
Christian Nuss <christian.nuss@gmail.com>
2020-10-13 21:23:04 -0700

    Switch to email verification for domains

--- aws-dns/main.tf
+++ aws-dns/main.tf
@@ -2,51 +2,56 @@ variable "stage" {}
 variable "domain" {}
 variable "delegation_set_id" {}
 
+locals {
+  domain = "sly.${var.domain}"
+}
+
 resource "aws_route53_zone" "zone" {
-  name              = var.domain
+  name              = local.domain
   delegation_set_id = var.delegation_set_id
 }
 
-resource "aws_acm_certificate" "apex" {
-  domain_name       = var.domain
-  validation_method = "DNS"
+resource "aws_acm_certificate" "certificate" {
+  domain_name               = "*.${local.domain}"
+  subject_alternative_names = [local.domain, "*.${var.domain}", var.domain, ]
+  validation_method         = "EMAIL"
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_acm_certificate" "subdomain_wildcard" {
-  domain_name       = "*.${var.domain}"
-  validation_method = "DNS"
+# Used to give AWS time to provision the new account
+resource "time_sleep" "wait_300_seconds" {
+  create_duration = "300s"
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  depends_on = [
+    aws_acm_certificate.subdomain_wildcard
+  ]
 }
 
-resource "aws_route53_record" "verification_record" {
-  for_each = {
-    for dvo in aws_acm_certificate.apex.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "aws_route53_record" "verification_record" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.apex.domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  name    = each.value.name
-  records = [each.value.record]
-  ttl     = 60
-  type    = each.value.type
-  zone_id = aws_route53_zone.zone.zone_id
-}
+#   name    = each.value.name
+#   records = [each.value.record]
+#   ttl     = 60
+#   type    = each.value.type
+#   zone_id = aws_route53_zone.zone.zone_id
+# }
 
 output "zone_id" {
   value = aws_route53_zone.zone.zone_id
 }
 
 output "domain" {
-  value = var.domain
+  value = local.domain
 }
 
 output "stage" {
--- output.tf
+++ output.tf
@@ -1,3 +1,5 @@
 output "main_nameservers" {
   value = module.dns.nameservers
 }
+
+// TODO Output NS Records to Create
