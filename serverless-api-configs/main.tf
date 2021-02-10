ab11b5d50d4036bbb2358f15630d2212c2bb84d9
Christian Nuss <christian.nuss@gmail.com>
2021-02-09 19:48:52 -0800

    Write one file

--- serverless-api-configs/main.tf
+++ serverless-api-configs/main.tf
@@ -1,6 +1,6 @@
 variable "service_name" {}
 variable "repository_name" {}
-variable "stage_config" {
+variable "stage_configs" {
   type = map(any)
 }
 
@@ -11,13 +11,13 @@ data "github_repository" "repository" {
 resource "github_repository_file" "serverless_apis" {
   repository = data.github_repository.repository.name
   branch     = data.github_repository.repository.default_branch
-  file       = ".scaffoldly/config/${var.service_name}.yml"
+  file       = ".scaffoldly/config/services.yml"
 
   content = templatefile("${path.module}/yaml.tpl", {
-    yaml = yamlencode(var.stage_config)
+    yaml = yamlencode(var.stage_configs)
   })
 
-  commit_message = "[Scaffoldly] Update config: ${var.service_name}.yml"
+  commit_message = "[Scaffoldly] Update config: services.yml"
   commit_author  = "Scaffoldly Bootstrap"
   commit_email   = "bootstrap@scaffold.ly"
 }
