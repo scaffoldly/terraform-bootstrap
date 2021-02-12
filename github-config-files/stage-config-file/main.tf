0fb0aa040cc2e6c31a6646028a9b640d6ad6a480
Christian Nuss <christian.nuss@gmail.com>
2021-02-11 21:06:29 -0800

    Sanitize service names, structure by environment

--- github-config-files/stage-config-file/main.tf
+++ github-config-files/stage-config-file/main.tf
@@ -5,15 +5,19 @@ variable "stage_config" {
   type = map(any)
 }
 
+locals {
+  scrubbed_service_name = replace(var.service_name, "-", "_")
+}
+
 resource "github_repository_file" "services" {
   for_each = var.stage_config
 
   repository = var.repository
   branch     = var.branch
-  file       = ".scaffoldly/.env/${var.service_name}.${each.key}.env"
+  file       = ".scaffoldly/.env/${each.key}/${var.service_name}.env"
 
   content = <<EOF
-  ${upper(var.service_name)}_${upper(each.key)}_URL=${lookup(each.value, "url", "")}
+  ${upper(local.scrubbed_service_name)}_${upper(each.key)}_URL=${lookup(each.value, "url", "")}
   EOF
 
   commit_message = "[Scaffoldly] Update env: ${var.service_name} ${each.key}"
