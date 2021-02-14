adbe72cc9466c9a9036451127b9b72a24db505f3
Christian Nuss <christian.nuss@gmail.com>
2021-02-13 17:22:58 -0800

    Remove trailing quote

--- github-config-files/stage-urls-files/main.tf
+++ github-config-files/stage-urls-files/main.tf
@@ -23,7 +23,7 @@ resource "github_repository_file" "env" {
   file       = ".scaffoldly/${var.stage_name}/service-urls.env"
 
   content = <<EOF
-service_urls=${jsonencode(var.stage_urls)}"
+service_urls=${jsonencode(var.stage_urls)}
 EOF
 
   commit_message = "[Scaffoldly] Update ${var.stage_name}/service-urls.env"
