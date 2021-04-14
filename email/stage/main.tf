cbf2a4b2cb34e64ddd62ad211015da8b57996236
Christian Nuss <christian.nuss@gmail.com>
2021-04-14 05:01:01 -0700

    Uppdate DKIM CNAME

--- email/stage/main.tf
+++ email/stage/main.tf
@@ -123,7 +123,7 @@ resource "aws_route53_record" "verification_record" {
 resource "aws_route53_record" "dkim_record" {
   count   = 3
   zone_id = var.dns_domain_id
-  name    = "${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}._domainkey"
+  name    = "${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}._domainkey.${var.domain}"
   type    = "CNAME"
   ttl     = "600"
   records = ["${element(aws_ses_domain_dkim.dkim.dkim_tokens, count.index)}.dkim.amazonses.com"]
