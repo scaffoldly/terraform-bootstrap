6a015147ab49b3cfdfe2783a7999df0dfe625cd4
Christian Nuss <christian.nuss@gmail.com>
2021-04-14 17:07:16 -0700

    Better default map

--- github-config-files/main.tf
+++ github-config-files/main.tf
@@ -47,7 +47,7 @@ module "stage_files" {
 
   env_vars = {
     for key, value in var.stage_env_vars :
-    key => lookup(value, var.stages[count.index], {})
+    key => lookup(value, var.stages[count.index], { UNKNOWN_ENV_VARS = "true" })
   }
 
   shared_env_vars = var.shared_env_vars
@@ -68,7 +68,7 @@ module "stage_files_default" {
 
   env_vars = {
     for key, value in var.stage_env_vars :
-    key => lookup(value, var.stages[count.index], {})
+    key => lookup(value, var.stages[count.index], { UNKNOWN_ENV_VARS = "true" })
   }
 
   shared_env_vars = var.shared_env_vars
