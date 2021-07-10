085e31d330bbb328ba1a499521e9510caefa8406
Christian Nuss <christian.nuss@gmail.com>
2021-07-09 18:13:52 -0700

    scaffoldly/stage-email/aws

--- main.tf
+++ main.tf
@@ -47,7 +47,8 @@ module "dns" {
 }
 
 module "email" {
-  source = "./email"
+  source  = "scaffoldly/stage-email/aws"
+  version = "0.15.1"
 
   root_email    = var.root_email
   stage_domains = module.dns.stage_domains
